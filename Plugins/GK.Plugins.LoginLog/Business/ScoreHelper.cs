c096d82778f84984f57075a489c96cf5455a3384
Volkan SERTER <volkan.serter@sahibinden.com>
2015-10-27 10:16:54 +0200

    .

---
+++ Plugins/GK.Plugins.LoginLog/Business/ScoreHelper.cs
@@ -0,0 +1,105 @@
+﻿using GK.Library.Utility;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace GK.Library.Business
+{
+    public static class ScoreHelper
+    {
+        public static MsCrmResultObject GetScoreLimitsByType(ScoreType scoreType, SqlDataAccess sda)
+        {
+
+            MsCrmResultObject returnValue = new MsCrmResultObject();
+            try
+            {
+                #region | SQL QUERY |
+                string query = @"SELECT
+	                                sl.new_scorelimitId AS Id
+	                                ,sl.new_name AS Name
+	                                ,sl.new_scoretype AS ScoreType
+	                                ,sl.new_frequency AS Frequency
+	                                ,sl.new_point AS Point
+	                                ,sl.new_scoreperiod AS Period
+                                FROM
+	                                new_scorelimit AS sl (NOLOCK)
+                                WHERE
+	                                sl.new_scoretype=@ScoreType --Sisteme giriş
+                                AND
+	                                sl.statecode=0
+                                AND
+	                                sl.statuscode=1 --Aktif
+                                ORDER BY
+	                                sl.new_scoreperiod ASC";
+                #endregion
+
+                SqlParameter[] parameters = {
+                                            new SqlParameter("@ScoreType",(int)scoreType)
+                                            };
+
+                DataTable dt = sda.getDataTable(query, parameters);
+
+
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    List<ScoreLimit> returnList = new List<ScoreLimit>();
+
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        ScoreLimit _scoreLimit = new ScoreLimit();
+                        _scoreLimit.Id = (Guid)dt.Rows[i]["Id"];
+                        _scoreLimit.Name = dt.Rows[i]["Name"] != DBNull.Value ? dt.Rows[i]["Name"].ToString() : string.Empty;
+                        _scoreLimit.ScoreType = (ScoreType)(int)dt.Rows[i]["ScoreType"];
+                        _scoreLimit.Period = (ScorePeriod)(int)dt.Rows[i]["Period"];
+                        _scoreLimit.Point = (int)dt.Rows[i]["Point"];
+                        _scoreLimit.Frequency = (int)dt.Rows[i]["Frequency"];
+
+                        returnList.Add(_scoreLimit);
+                    }
+
+                    returnValue.Success = true;
+                    returnValue.ReturnObject = returnList;
+                }
+                else
+                {
+                    returnValue.Success = false;
+                    returnValue.Result = "Skor Limit kaydı bulunamadı.";
+                }
+            }
+            catch (Exception ex)
+            {
+                returnValue.Success = false;
+                returnValue.Result = ex.Message;
+            }
+            return returnValue;
+        }
+
+        public static MsCrmResult CreateScore(Score score, IOrganizationService service)
+        {
+            MsCrmResult returnValue = new MsCrmResult();
+
+            try
+            {
+                Entity ent = new Entity("new_score");
+                ent["new_name"] = score.ScoreType.ToString() + "-" + score.User.Name + "-" + DateTime.Now.ToString("dd.MM.yyyy HH:mm");
+                ent["new_portalid"] = score.Portal;
+                ent["new_userid"] = score.User;
+                ent["new_point"] = score.Point;
+                ent["new_scoretype"] = new OptionSetValue((int)score.ScoreType);
+
+                returnValue.CrmId = service.Create(ent);
+                returnValue.Success = true;
+            }
+            catch (Exception ex)
+            {
+                returnValue.Result = ex.Message;
+            }
+
+            return returnValue;
+        }
+    }
+}
---
+++ Plugins/GK.Plugins.VideoLog/PostCreate.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using GK.Library.Business;
+using GK.Library.Utility;
+
+namespace GK.Plugins.VideoLog
+{
+    public class PostCreate : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            SqlDataAccess sda = null;
+
+            try
+            {
+                sda = new SqlDataAccess();
+                sda.openConnection(Globals.ConnectionString);
+
+                #region | SERVICE |
+                IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+                #region | Validate Request |
+                //Target yoksa veya Entity tipinde değilse, devam etme.
+                if (!context.InputParameters.Contains("Target") || !(context.InputParameters["Target"] is Entity))
+                {
+                    return;
+                }
+                #endregion
+
+                IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                #endregion
+
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                #region | VARIABLES |
+                List<ScoreLimit> lstLimits = new List<ScoreLimit>();
+
+                EntityReference portal = null;
+                EntityReference user = null;
+
+                if (entity.Contains("new_portalid") && entity["new_portalid"] != null)
+                {
+                    portal = (EntityReference)entity["new_portalid"];
+                }
+
+                if (entity.Contains("new_userid") && entity["new_userid"] != null)
+                {
+                    user = (EntityReference)entity["new_userid"];
+                }
+                #endregion
+
+                MsCrmResultObject limitRes = ScoreHelper.GetScoreLimitsByType(ScoreType.EditVideo, sda);
+
+                if (limitRes.Success)
+                {
+                    lstLimits = (List<ScoreLimit>)limitRes.ReturnObject;
+
+                    for (int i = 0; i < lstLimits.Count; i++)
+                    {
+                        int recCount = 0;
+                        DateTime start = GeneralHelper.GetStartDateByScorePeriod(lstLimits[i].Period);
+                        DateTime end = GeneralHelper.GetEndDateByScorePeriod(lstLimits[i].Period);
+
+                        recCount = VideoHelper.GetVideoEditCount(portal.Id, user.Id, start, end, sda);
+
+                        if (lstLimits[i].Frequency >= recCount)
+                        {
+                            Score sc = new Score()
+                            {
+                                Point = lstLimits[i].Point,
+                                Portal = portal,
+                                User = user,
+                                ScoreType = ScoreType.EditVideo
+                            };
+
+                            MsCrmResult scoreRes = ScoreHelper.CreateScore(sc, service);
+
+                            break;
+                        }
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                //LOG
+                throw new InvalidPluginExecutionException(ex.Message);
+            }
+            finally
+            {
+                if (sda != null)
+                    sda.closeConnection();
+            }
+        }
+    }
+}
